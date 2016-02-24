5edd5c1cdcca744f116eba39258e34de83394bb8
kuaichuan <meidong@meidongmacbook-air.local>
2016-02-24 14:19:02 +0800

    Initial Commit

---
+++ VideoHandle/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoHandle
+//
+//  Created by 没懂 on 16/2/24.
+//  Copyright © 2016年 com.comelet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ VideoHandle/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  VideoHandle
+//
+//  Created by 没懂 on 16/2/24.
+//  Copyright © 2016年 com.comelet. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
